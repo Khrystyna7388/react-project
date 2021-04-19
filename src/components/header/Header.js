e8b1aafe646b73aa8f927ca5d2a7218487426096
Khrystyna7388 <khristina7388@gmail.com>
2021-04-19 15:26:50 +0300

    project

---
+++ src/components/header/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './Header.css';
+import {CART_URL, WISHLIST_URL, CART_ALT, WISHLIST_ALT} from "../types";
+
+
+export const Header = () => {
+
+    return(
+        <header>
+            <h2>PRODUCTS STORE</h2>
+
+            <div>
+                <div>
+                    <img className="cart-image"
+                         src={CART_URL} alt={CART_ALT} />
+                    <span> 0</span>
+                </div>
+
+                <div>
+                    <img className="wishlist-image"
+                         src={WISHLIST_URL} alt={WISHLIST_ALT} />
+                    <span>0</span>
+                </div>
+            </div>
+        </header>
+    )
+}
---
+++ src/components/types/alt.js
@@ -0,0 +1,7 @@
+const CART_ALT = 'cart';
+const WISHLIST_ALT = 'wishlist';
+
+export {
+    CART_ALT,
+    WISHLIST_ALT
+}
